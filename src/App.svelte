625db3e9147e0a83dfe938ae02a7625da183b845
Josue <josue.almonasi@hotmail.com>
2019-10-12 19:36:51 -0500

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
+  let done = false;
+
+  function addContact() {
+    done = !done;
+  }
+</script>
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+<button on:click={addContact}>Add contact</button>
+
+{#if done}
+  <ContactCard
+    userName={name}
+    jobTitle={title}
+    {description}
+    userImage={image} />
+{/if}
